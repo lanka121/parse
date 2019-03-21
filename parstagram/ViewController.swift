8613cdf682ad4c1cc9bc7b70b959c90157762d2a
lanka vidanapathirana <lankavidanapathirana@lankas-macbook-air.local>
2019-03-21 17:42:50 -0400

    parstagram

---
+++ parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parstagram
+//
+//  Created by lanka vidanapathirana on 3/20/19.
+//  Copyright © 2019 lanka vidanapathirana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
